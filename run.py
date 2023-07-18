79fe9c53c89ad2890681b69144bcd7d101f9121c
Danilo Martins <danvm94@gmail.com>
2023-07-18 13:21:41 +0100

    Initial structure added to run.py, with the 2 initial functions check_db_presence and main.

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# sqlite3 used to store the username and passwords information
+import sqlite3
+# os library to check if file exists
+import os
+
+# Specify the path and filename of the SQLite database file
+DATABASE = '.\passwords.db'
+
+def check_db_presence(database):
+    """
+    Check the presence of a database file at the specified path.
+    Args:
+        database (str): The path and filename of the database file.
+    Returns:
+        bool: True if the database file exists, False otherwise.
+    """
+    if not os.path.exists(database):
+        print(f"There is no database file at the path provided: {database}")
+        return False
+    else:
+        return True
+
+def main():
+    pass
+
