00d3053e57a2f05e37a91ae3a9e6e63e9fc8797b
Danilo Martins <danvm94@gmail.com>
2023-07-20 19:40:11 +0100

    Refactor create_db() to use an in-memory SQLite database
    Previously, create_db() created a local SQLite file to store the credentials. This commit refactors the create_db() function to use an in-memory SQLite database instead. The credentials are now stored in memory, and no data is persisted beyond the lifetime of the application. This change ensures that no sensitive data is stored on disk, enhancing security and privacy.

--- run.py
+++ run.py
@@ -24,7 +24,7 @@ def check_db_presence(database_path):
     else:
         print(f"Database presence confirmed: {database_path}.\n")
 
-def create_db(database_path):
+def create_db():
     """
     Create a new SQLite database with a 'credentials' table.
     Args:
@@ -32,26 +32,14 @@ def create_db(database_path):
     Returns:
         None
     """
-    print(f"Creating: {database_path}.\n")
-    conn = sqlite3.connect(database_path)
-    cursor = conn.cursor()
+    print(f"Creating database on computer's memory.\n")
+    connector = sqlite3.connect(":memory:")
+    cursor = connector.cursor()
     cursor.execute(f"""CREATE TABLE IF NOT EXISTS {TABLE}
                   (id INTEGER PRIMARY KEY AUTOINCREMENT,
                   username TEXT NOT NULL,
                   password TEXT NOT NULL,
                   service TEXT NOT NULL);""")
-    conn.close()
-
-def connect_db(database_path):
-    """
-    Establish a connection to an SQLite database.
-    Args:
-        database_path (str): The path and filename of the database file.
-    Returns:
-        sqlite3.Connection: The connection object for the SQLite database.
-    """
-    connector = sqlite3.connect(database_path)
-
     return connector
 
 def get_column_names(database):
@@ -256,8 +244,7 @@ def print_menu(options):
 
 def main():
     print("Welcome to LockMinder\n")
-    check_db_presence(DATABASE_PATH)
-    database = connect_db(DATABASE_PATH)
+    database = create_db()
     display_menu(database)
 
 main()
